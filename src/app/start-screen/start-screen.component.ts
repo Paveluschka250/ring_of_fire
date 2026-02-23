61bbb4c6fc81cbd785b04f69a0efad5012d15182
Paul Gross <p.gross6408@gmail.com>
2026-02-23 11:22:02 +0100

    feat: Enhance start game functionality with loading state and error handling using MatSnackBar

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Game } from 'src/models/game';
 
 @Component({
@@ -9,21 +10,35 @@ import { Game } from 'src/models/game';
   styleUrls: ['./start-screen.component.scss']
 })
 export class StartScreenComponent implements OnInit {
+  starting = false;
+
   constructor(
     private router: Router,
-    private firestore: AngularFirestore
+    private firestore: AngularFirestore,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
   }
 
   startGame() {
+    if (this.starting) return;
+    this.starting = true;
     const game = new Game();
     this.firestore
       .collection('games')
       .add(game.toJson())
       .then((gameInfo: any) => {
         this.router.navigateByUrl('/game/' + gameInfo.id);
+      })
+      .catch((err) => {
+        this.starting = false;
+        console.error('Fehler beim Erstellen des Spiels:', err);
+        this.snackBar.open(
+          'Spiel konnte nicht gestartet werden. Bitte prüfe die Verbindung und lade die Seite neu.',
+          'OK',
+          { duration: 8000 }
+        );
       });
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'YOUR_API_KEY',
-    authDomain: 'YOUR_AUTH_DOMAIN',
-    projectId: 'YOUR_PROJECT_ID',
-    storageBucket: 'YOUR_STORAGE_BUCKET',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    appId: 'YOUR_APP_ID'
+    apiKey: 'AIzaSyBndhRJnsWYDB5RPdwCWM64kEqgGSccWgo',
+    authDomain: 'ring-of-fire-f0b45.firebaseapp.com',
+    databaseURL: 'https://ring-of-fire-f0b45-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'ring-of-fire-f0b45',
+    storageBucket: 'ring-of-fire-f0b45.firebasestorage.app',
+    messagingSenderId: '895374363316',
+    appId: '1:895374363316:web:9713451b7a11a48f8565e1'
   }
 };
 
